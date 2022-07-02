a8769430da552651bfe40a5f01dfbc854632ca18
Jared <jared.wilson4332@gmail.com>
2022-07-01 21:56:11 -0600

    Add temp conversion and display

--- src/index.js
+++ src/index.js
@@ -2,14 +2,49 @@ import './style.css';
 
 const getWeather = async () => {
     try {
-      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&APPID=435784523dbad895916dd547dcc4f17f', {mode: 'cors'})
-      const responseData = await response.json()
-      console.log(responseData)
+        const city = getSearch()
+    //    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=435784523dbad895916dd547dcc4f17f`, {mode: 'cors'})
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=malmo&APPID=435784523dbad895916dd547dcc4f17f`, {mode: 'cors'})
+        const weatherData = await response.json()
+        if (weatherData.cod == '404') {
+            throw 'Not a valid city'
+        }
+        const newTemp = tempConvert(weatherData.main.temp, 'c')
+        displayWeather(newTemp)
     }
 
     catch(error) {
-      alert("No weather")
+        console.log(error)
     }
 }
+
+const getSearch = () => {
+    let city = search.value
+    city.split('').forEach(letter => {
+        if (letter === " ") {
+          letter = "+"
+        }
+      });
+      return city
+}
+
+const tempConvert = (num, temp) => {
+    let value
+    if (temp == "c") {
+        value = (num - 273.15)
+    } else if (temp == "f") {
+        value = (1.8 *(num - 273) + 32)
+    }
+    return value.toFixed(2)
+}
+
+const displayWeather = (temp) => {
+    const weather = document.createElement('h1')
+    weather.textContent = temp
+    content.appendChild(weather)
+}
+
+
+searchButton.addEventListener("click", getWeather)
 
 getWeather()
