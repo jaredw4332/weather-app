ec88c5dd99eee6d1f94a668a8f5f88cb30f74c26
Jared <jared.wilson4332@gmail.com>
2022-07-03 13:47:12 -0600

    Add temperature conversion button, add city name display

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './style.css';
 
 const getWeather = async () => {
     try {
-        const city = getSearch()
-    //    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=435784523dbad895916dd547dcc4f17f`, {mode: 'cors'})
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=regina&APPID=435784523dbad895916dd547dcc4f17f`, {mode: 'cors'})
+        let city = getSearch()
+        if (city == '') { city = 'Manila' }
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=435784523dbad895916dd547dcc4f17f`, {mode: 'cors'})
         const weatherData = await response.json()
         if (weatherData.cod == '404') {
             throw 'Not a valid city'
@@ -19,15 +19,15 @@ const getWeather = async () => {
 }
 
 const displayHandler = (weather, cityName) => {
-    const newTemp = tempConvert(weather.main.temp, 'c')
+    const newTemp = tempConvertK(weather.main.temp, 'C')
     const tempDesc = weather.weather[0].description
     const tempIcon = weather.weather[0].icon
     cityName = titleString(cityName)
     displayWeather(newTemp, tempDesc, tempIcon, cityName)
 
-    const tempFeel = tempConvert(weather.main.feels_like, 'c')
-    const minTemp = tempConvert(weather.main.temp_min, 'c')
-    const maxTemp = tempConvert(weather.main.temp_max, 'c')
+    const tempFeel = tempConvertK(weather.main.feels_like, 'C')
+    const minTemp = tempConvertK(weather.main.temp_min, 'C')
+    const maxTemp = tempConvertK(weather.main.temp_max, 'C')
     displayAltWeather(tempFeel, minTemp, maxTemp, weather.main.humidity)
 
     displayWind(weather.wind)
@@ -43,36 +43,64 @@ const getSearch = () => {
       return city
 }
 
-const tempConvert = (num, temp) => {
+const tempConvertK = (num, temp) => {
     let value
-    if (temp == "c") {
+    if (temp == "C") {
         value = (num - 273.15).toFixed(0)
         value = `${value}°C`
         return value
-    } else if (temp == "f") {
+    } else if (temp == "F") {
         value = (1.8 *(num - 273) + 32).toFixed(0)
         value = `${value}°F`
         return value
     }
 }
 
-// doesnt work
-const titleString = (string) => {
-    for (let spot in string) {
-        if ([spot] === 0) {
-            string.charAt(spot).toUpperCase() + string.slice(1)
-        } else {
-            string.charAt(spot).toLowerCase() + string.slice(1)
-        }
+const tempConvertCF = (num, current) => {
+    let value
+    if (current == "C") {
+        value = ((num * (9 / 5)) + 32).toFixed(0)
+        value = `${value}°F`
+        return value
+    } else if (current == "F") {
+        value = ((num - 32) * (5 / 9)).toFixed(0)
+        value = `${value}°C`
+        return value
     }
+}
+
+const switchTemp = () => {
+    const tempCollection = document.getElementsByClassName('degrees')
+    for (let item in tempCollection) {
+        if (item == 'length') {
+            return
+        }
+        let num = tempCollection[item].textContent
+        let temp = num.substring(num.length-1)
+
+        num = num.slice(0, -2)
+        num = Number(num)
+        num = tempConvertCF(num, temp)
+
+        tempCollection[item].textContent = num
+    }
+}
+
+tempSwitch.addEventListener("click", switchTemp)
+
+const titleString = (string) => {
+    string = string.toLowerCase()
+    string = string.split('')
+    string[0] = string[0].toUpperCase()
+    string = string.join('')
     return string
 }
 
 // celcius to fahrenheit swap button to grab degree class, cut off last 2 characters,
-// convert to number and run it through tempConvert with an f
+// convert to number and run it through tempConvertK with an f
 
 const displayWeather = (temp, tempDesc, tempIcon, cityName) => {
-    displayThing('cityName', 'p', 'Temporary', 0, weatherBox)
+    displayThing('cityName', 'p', cityName, 0, weatherBox)
     displayThing('tempInfo', 'h1', temp, 'degrees', weatherBox)
     displayThing('tempDesc', 'p', tempDesc, 0, weatherBox)
     displayThing(tempIcon, 'div', 0, 'icon', weatherBox)
@@ -94,6 +122,9 @@ const displayWind = (wind) => {
 }
 
 const displayThing = (name, type, text, group, parent) => {
+    if (document.getElementById(name)) {
+        document.getElementById(name).remove()
+    }
     if (text == undefined) {
         parent.style.display = 'none'
         return
